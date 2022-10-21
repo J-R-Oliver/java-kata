3d05500d5f5557feef032cad3c74d9c633179b9d
James Oliver <james.oliver@engineering.digital.dwp.gov.uk>
2022-10-21 11:56:29 +0100

    docs: adds java doc to camelcase

--- src/main/java/dev/jamesoliver/kata/breakcamelcase/Kata.java
+++ src/main/java/dev/jamesoliver/kata/breakcamelcase/Kata.java
@@ -8,6 +8,12 @@ public class Kata {
 
   private Kata() {}
 
+  /**
+   * Adds space before every capital letter.
+   *
+   * @param input string to transform
+   * @return transformed string
+   */
   public static String camelCase(String input) {
     return input.replaceAll("([A-Z])", " $1");
   }
