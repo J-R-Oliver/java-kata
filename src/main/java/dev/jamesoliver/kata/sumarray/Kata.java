3f1e3803610b0b15e7efb64bc6c23876e5da34be
James Oliver <james.oliver@engineering.digital.dwp.gov.uk>
2022-10-25 10:45:34 +0100

    feat: adds sum array kata

---
+++ src/main/java/dev/jamesoliver/kata/sumarray/Kata.java
@@ -0,0 +1,17 @@
+package dev.jamesoliver.kata.sumarray;
+
+import java.util.Arrays;
+
+/**
+ * Write a function that takes an array of numbers and returns the sum of the numbers. The numbers
+ * can be negative or non-integer. If the array does not contain any numbers then you should return
+ * 0.
+ */
+public class Kata {
+
+  private Kata() {}
+
+  public static double sum(double[] numbers) {
+    return Arrays.stream(numbers).sum();
+  }
+}
