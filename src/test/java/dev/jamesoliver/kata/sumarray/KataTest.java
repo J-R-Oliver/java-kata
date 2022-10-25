da8e8417a51100c0a7bdf1b34e929f0e3934b4be
James Oliver <james.oliver@engineering.digital.dwp.gov.uk>
2022-10-25 10:48:30 +0100

    style: formats file

--- src/test/java/dev/jamesoliver/kata/sumarray/KataTest.java
+++ src/test/java/dev/jamesoliver/kata/sumarray/KataTest.java
@@ -3,7 +3,7 @@ package dev.jamesoliver.kata.sumarray;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Sum Arrays")
 class KataTest {
@@ -11,29 +11,30 @@ class KataTest {
   @Test
   @DisplayName("Given an array of positive numbers then returns the sum of the numbers")
   void givenAnArrayOfPositiveNumbersThenReturnsTheSumOfTheNumbers() {
-    assertEquals(10, Kata.sum(new double[]{1, 2, 3, 4}));
+    assertEquals(10, Kata.sum(new double[] {1, 2, 3, 4}));
   }
 
   @Test
   @DisplayName("Given an array of negative numbers then returns the sum of the numbers")
   void givenAnArrayOfNegativeNumbersThenReturnsTheSumOfTheNumbers() {
-    assertEquals(-10, Kata.sum(new double[]{-1, -2, -3, -4}));
+    assertEquals(-10, Kata.sum(new double[] {-1, -2, -3, -4}));
   }
 
   @Test
-  @DisplayName("Given an array of positive and negative numbers then returns the sum of the numbers")
+  @DisplayName(
+      "Given an array of positive and negative numbers then returns the sum of the numbers")
   void givenAnArrayOfPositiveAndNegativeNumbersThenReturnsTheSumOfTheNumbers() {
-    assertEquals(2, Kata.sum(new double[]{-1, 2, -3, 4}));
+    assertEquals(2, Kata.sum(new double[] {-1, 2, -3, 4}));
   }
 
   @Test
   @DisplayName("Code Wars examples")
   void codeWarsExamples() {
-    assertEquals(0,      Kata.sum(new double[] { }),                1e-4);
-    assertEquals(-2.398, Kata.sum(new double[] {-2.398}),           1e-4);
-    assertEquals(6,      Kata.sum(new double[] {1, 2, 3}),          1e-4);
-    assertEquals(6.6,    Kata.sum(new double[] {1.1, 2.2, 3.3}),    1e-4);
-    assertEquals(9.2,    Kata.sum(new double[] {1, 5.2, 4, 0, -1}), 1e-4);
-    assertEquals(320,    Kata.sum(new double[] {30, 89, 100, 101}), 1e-4);
+    assertEquals(0, Kata.sum(new double[] {}), 1e-4);
+    assertEquals(-2.398, Kata.sum(new double[] {-2.398}), 1e-4);
+    assertEquals(6, Kata.sum(new double[] {1, 2, 3}), 1e-4);
+    assertEquals(6.6, Kata.sum(new double[] {1.1, 2.2, 3.3}), 1e-4);
+    assertEquals(9.2, Kata.sum(new double[] {1, 5.2, 4, 0, -1}), 1e-4);
+    assertEquals(320, Kata.sum(new double[] {30, 89, 100, 101}), 1e-4);
   }
 }
